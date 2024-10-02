3b15f4569030539842f8f246e009dc249ed40e96
websitemovies09 <websitemovies09@gmail.com>
2024-10-02 20:32:35 +0700

    a
    s

--- app/api/movies/caterogy/[id]/route.js
+++ app/api/movies/caterogy/[id]/route.js
@@ -1,47 +1,37 @@
 import connection from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request, params) {
+export async function GET(request,params) {
   const searchParams = request.nextUrl.searchParams;
   try {
     const page = parseInt(searchParams.get("page")) || 1;
     const perPage = parseInt(searchParams.get("limit")) || 40; // Số mục trên mỗi trang
     const offset = (page - 1) * perPage;
 
-    // Lấy giá trị `caterogy_id` từ params
-    const caterogyId = parseInt(params.params.id);
+    // Lấy giá trị `caterogy_id` từ searchParams
+    const caterogyId = parseInt(params.params.id)
 
     // Truy vấn để lấy tổng số mục có điều kiện caterogy_id
-    let countQuery = `SELECT COUNT(*) as totalCount FROM movies WHERE active = 1`;
-    let countParams = [];
-    
-    if (caterogyId) {
-      countQuery += ` AND caterogy_id = ?`;
-      countParams.push(caterogyId);
-    }
-    
-    const [countResults] = await connection.execute(countQuery, countParams);
+    const countQuery = `
+      SELECT COUNT(*) as totalCount
+      FROM movies
+      WHERE active = 1 ${caterogyId ? `AND caterogy_id = ?` : ""}
+    `;
+    const [countResults] = await connection.execute(countQuery, [caterogyId]);
     const totalCount = countResults[0].totalCount;
     const totalPages = Math.ceil(totalCount / perPage);
-
+    
     // Truy vấn dữ liệu
-    let query = `
+    let text  =caterogyId ? `AND movies.caterogy_id = ?` : ""
+    const query = `
       SELECT movies.*, caterogys.title AS category
       FROM movies
       JOIN caterogys ON movies.caterogy_id = caterogys.id
-      WHERE movies.active = 1
+      WHERE movies.active = 1 ${text}
+      ORDER BY movies.id DESC
+      LIMIT ?, ?
     `;
-    let queryParams = [];
-    
-    if (caterogyId) {
-      query += ` AND movies.caterogy_id = ?`;
-      queryParams.push(caterogyId);
-    }
-
-    query += ` ORDER BY movies.id DESC LIMIT ?, ?`;
-    queryParams.push(offset, perPage);
-
-    const [results] = await connection.execute(query, queryParams);
+    const [results] = await connection.execute(query, [caterogyId, offset, perPage]);
 
     return NextResponse.json(
       { movies: results, totalPages, totalCount, perPage },
