7d87322b4f331cbc28ee008a02a0ac26c2efd349
websitemovies09 <websitemovies09@gmail.com>
2024-10-03 10:46:03 +0700

    search sql
    search sql

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -2,12 +2,6 @@ import connection from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  // const allowedOrigins = process.env.NEXT_BASE_URL;
-  // const origin = request.nextUrl.origin;
-  // const isAllowed = origin.includes(allowedOrigins);
-  // if (!isAllowed) {
-  //   return NextResponse.json({ message: allowedOrigins }, { status: 403 });
-  // }
   const searchParams = request.nextUrl.searchParams;
   try {
     const page = parseInt(searchParams.get("page")) || 1;
@@ -17,14 +11,14 @@ export async function GET(request) {
 
     // Truy vấn để lấy tổng số mục có điều kiện tìm kiếm
     const countQuery = `
-      SELECT COUNT(*) as totalCount
-      FROM movies
-      WHERE active = 1 ${searchQuery ? `AND title LIKE ?` : ""}
-    `;
-    const [countResults] = await connection.execute(countQuery, [`%${searchQuery}%`]);
+  SELECT COUNT(*) as totalCount
+  FROM movies
+  WHERE active = 1 ${searchQuery ? `AND title LIKE '%${searchQuery}%'` : ""}
+`;
+    const [countResults] = await connection.execute(countQuery);
     const totalCount = countResults[0].totalCount;
     const totalPages = Math.ceil(totalCount / perPage);
-    
+
     // Truy vấn dữ liệu
     const query = `
       SELECT movies.*, caterogys.title AS category
@@ -34,7 +28,11 @@ export async function GET(request) {
       ORDER BY movies.id DESC
       LIMIT ?, ?
     `;
-    const [results] = await connection.execute(query, [`%${searchQuery}%`, offset, perPage]);
+    const [results] = await connection.execute(query, [
+      `%${searchQuery}%`,
+      offset,
+      perPage,
+    ]);
 
     return NextResponse.json(
       { movies: results, totalPages, totalCount, perPage },
