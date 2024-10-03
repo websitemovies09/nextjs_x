2f0d00ecbf73a227d76c06634a024d99b967cfd1
websitemovies09 <websitemovies09@gmail.com>
2024-10-03 10:34:10 +0700

    err cate
    err cate

--- redux_query/caterogy/caterogyApi.js
+++ redux_query/caterogy/caterogyApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const caterogyApi = createApi({
   reducerPath: 'caterogyApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }), 
+  baseQuery: fetchBaseQuery({ baseUrl: '/api' }), 
   endpoints: (builder) => ({
     getCaterogys: builder.query({
       query: () => 'caterogys', 
